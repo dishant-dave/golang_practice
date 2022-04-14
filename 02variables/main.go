aa66c6ed79b6a9ed861c45dbc69f5a55635d359a
Dishant Dave <dishant@prismetric.com>
2022-04-14 14:35:31 +0530

    02variables

---
+++ 02variables/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var username string = "dishant"
+	// fmt.Println(username)
+	// fmt.Printf("type username is %T \n", username)
+
+	// var isloggedin bool = true
+	var smallval uint8 = 255
+	fmt.Println(smallval)
+	fmt.Printf("type username is %T \n", smallval)
+	// no variable
+	hahaha := "asd"
+	fmt.Println(hahaha)
+}
